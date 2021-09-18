293a7820e882651c53f954ea78d2005949c1c5b8
EvgrafovBTW <bleedthemwhite@yandex.ru>
2021-09-18 18:56:22 +0700

    dfghhgf

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 //import { Component } from "react";
-import TodoList from "./Todo/TodoList";
+//import TodoList from "./Todo/TodoList";
 
 
 
 
 function App() {
 
-  const todos = [
+  /*const todos = [
     {id: 1, completed: false, title: "купить хуй"},
     {id: 1, completed: false, title: "купить ,kznm"},
     {id: 1, completed: false, title: "купить kjvfrbyf"}
-  ]
+  ]*/
   return (
-    <div className="wrapper" >
-      <h1>ЛОМАКИН ХУЙ</h1>
-
-      <TodoList todos={todos}/>
+    <div className="main" >
+      
     </div>
   );
 }
